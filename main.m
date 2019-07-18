51f7edbaeab3fe42d0543a6e27898e87ecef4e39
songyiliao <songyiliao@gmail.com>
2019-07-18 10:38:18 +1000

    delete comments

--- main.m
+++ main.m
@@ -1,6 +1,19 @@
 clear;clc
 % load SynData.mat; 
 load RealStreams.mat;
+for i = 1:8
+    data = database(i).Data;
+    [DATA{1,i},R{1,i}] = FUZZ_CARE(data,para);
+    X = ['Data:',database(i).Name,'  ','MAE:',num2str(mean(R{1,i}.MAE))];
+    disp(X)
+end 
+
+
+
+
+
+
+
 % Parameters
 % para.w = 200; %window size
 % para.TrN=2000; % historical data
@@ -12,12 +25,3 @@ load RealStreams.mat;
 % para.alpha = 0.01;
 % para.lambda2 = 0.5;
 % para.lambda1 = 1;
-
-
-for i = 1:8
-    data = database(i).Data;
-    [DATA{1,i},R{1,i}] = FUZZ_CARE(data,para);
-    X = ['Data:',database(i).Name,'  ','MAE:',num2str(mean(R{1,i}.MAE))];
-    disp(X)
-end 
-
